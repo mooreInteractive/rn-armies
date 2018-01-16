09b478bfd2933663f44145f724a4fb611ce7ca8f
Brett Sanders <brett.sanders@vevo.com>
2018-01-16 10:31:13 -0800

    capitalize Home.js for import

---
+++ src/components/Home.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { View, Text, Button } from "react-native";
+
+class Home extends Component {
+    render() {
+        const { navigate } = this.props.navigation;
+
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
+            >
+                <Text>Home Screen</Text>
+                <Button
+                    onPress={() => navigate("GroupPage")}
+                    title="Go to Groups"
+                />
+                <Button
+                    onPress={() => navigate("Exchange")}
+                    title="Go to exchange"
+                />
+                <Button onPress={() => navigate("FTUE")} title="Go to FTUE" />
+                <Button
+                    onPress={() => navigate("Wallet")}
+                    title="Go to wallet"
+                />
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    groups: state.groups
+});
+
+export default connect(mapStateToProps)(Home);
