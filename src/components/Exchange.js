33f2eda34bbe3dde1795cd8a6e91ee3631b5332c
Victor Mu <vic.mgs@gmail.com>
2018-01-16 17:37:47 -0800

    CSS work

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { ScrollView, View, Text, Button, Picker, TextInput, Modal, KeyboardAvoidingView, TouchableWithoutFeedback, StyleSheet } from "react-native";
+import { ScrollView, View, Image, Text, Button, Picker, TextInput, Modal, KeyboardAvoidingView, TouchableWithoutFeedback, StyleSheet } from "react-native";
 
 class Exchange extends Component {
   constructor(props) {
@@ -22,34 +22,48 @@ class Exchange extends Component {
       <ScrollView style={styles.container}>
         <Text style={styles.header}>Vevo Coin Exchange</Text>
 
-        <Text style={styles.subHeader}>Your Coins:</Text>
-        <Text style={styles.numInput} onPress={() => this.setState({ modalVisible: true, modalFocusTarget: "coinOwned" })}>
-          {this.state.coinOwned}
-        </Text>
-        <TextInput
-          style={styles.numInput}
-          onChangeText={(coinOwnedCount) => this.setState({coinOwnedCount})}
-          value={this.state.coinOwnedCount}
-          keyboardType="numeric"
-        />
-
-        <Text style={styles.subHeader} >Available Coins:</Text>
-        <Text style={styles.numInput} onPress={() => this.setState({ modalVisible: true, modalFocusTarget: "coinDesired" })}>
-          {this.state.coinDesired}
-        </Text>
-        <TextInput
-          style={styles.numInput}
-          onChangeText={(coinDesiredCount) => this.setState({coinDesiredCount})}
-          value={this.state.coinDesiredCount}
-          keyboardType="numeric"
-        />
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+          <View style={{width: 140, margin: 10}}>
+            <Text style={styles.subHeader}>Your Coins:</Text>
+            <Text style={styles.numInput} onPress={() => this.setState({ modalVisible: true, modalFocusTarget: "coinOwned" })}>
+              {this.state.coinOwned}
+            </Text>
+            <TextInput
+              style={styles.numInput}
+              onChangeText={(coinOwnedCount) => this.setState({coinOwnedCount})}
+              value={this.state.coinOwnedCount}
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={{marginTop: 50}}>
+            <Image
+              style={{width: 30, height: 30}}
+              source={require("../../assets/icons/arrow-flat.png")}
+            />
+          </View>
+          <View style={{width: 140, margin: 10}}>
+            <Text style={styles.subHeader} >Available Coins:</Text>
+            <Text style={styles.numInput} onPress={() => this.setState({ modalVisible: true, modalFocusTarget: "coinDesired" })}>
+              {this.state.coinDesired}
+            </Text>
+            <TextInput
+              style={styles.numInput}
+              onChangeText={(coinDesiredCount) => this.setState({coinDesiredCount})}
+              value={this.state.coinDesiredCount}
+              keyboardType="numeric"
+            />
+          </View>
+        </View>
 
         <Text style={styles.subHeader}>You have XX {this.state.coinOwned} in your wallet</Text>
-        <Text style={styles.buttonTitle}>% to Exchange:</Text>
-        <Button title="25%" onPress={() => this.setState({ coinOwnedCount: "25" })}/>
-        <Button title="50%" onPress={() => this.setState({ coinOwnedCount: "50" })}/>
-        <Button title="75%" onPress={() => this.setState({ coinOwnedCount: "75" })}/>
-        <Button title="100%" onPress={() => this.setState({ coinOwnedCount: "100" })}/>
+
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', margin: 20}}>
+          <Text style={styles.buttonTitle}>% to Exchange:</Text>
+          <Button title="25%" onPress={() => this.setState({ coinOwnedCount: "25" })}/>
+          <Button title="50%" onPress={() => this.setState({ coinOwnedCount: "50" })}/>
+          <Button title="75%" onPress={() => this.setState({ coinOwnedCount: "75" })}/>
+          <Button title="100%" onPress={() => this.setState({ coinOwnedCount: "100" })}/>
+        </View>
 
         <Text style={styles.subHeader}>You are exchanging {this.state.coinOwnedCount} {this.state.coinOwned} for XX {this.state.coinDesired}</Text>
         <Button
@@ -105,21 +119,22 @@ const styles = StyleSheet.create({
     },
     subHeader: {
         color: "#ffffff",
-        fontSize: 18,
+        fontSize: 16,
         fontWeight: "bold"
     },
     buttonTitle: {
         color: "#ffffff",
-        fontSize: 16,
-        fontWeight: "bold"
+        fontSize: 17,
+        paddingTop: 9
     },
     numInput: {
         color: "#ffffff",
         fontSize: 16,
         borderWidth: 1,
         borderColor: "#ffffff",
-        marginTop: 10,
-        marginBottom: 10
+        marginTop: 5,
+        marginBottom: 5,
+        paddingLeft: 5
     },
     text: {
         color: "#ffffff"
