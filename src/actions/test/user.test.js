4520d0b68bb170e41133ca17ab754261b3d40c8e
Brett Sanders <brett.sanders@vevo.com>
2018-01-18 14:05:27 -0800

    add tests for user actions

---
+++ src/actions/test/user.test.js
@@ -0,0 +1,51 @@
+import * as actions from "../user.js";
+
+describe("user actions", () => {
+    describe("addCoinToWallet", () => {
+        it("should create action to add coin to wallet", () => {
+            const coin = { key: "belieber", amount: 100 }
+            const expectedAction = {
+                type: actions.ADD_COIN,
+                key: coin.key,
+                amount: coin.amount
+            }
+            const resultAction = actions.addCoinToWallet(coin.key, coin.amount);
+            expect(resultAction).toEqual(expectedAction);
+        })
+    })
+
+    describe("subtractCoinFromWallet", () => {
+        it("should create action to subtract coin to wallet", () => {
+            const coin = { key: "belieber", amount: 100 }
+            const expectedAction = {
+                type: actions.SUBTRACT_COIN,
+                key: coin.key,
+                amount: coin.amount
+            }
+            const resultAction = actions.subtractCoinFromWallet(coin.key, coin.amount);
+            expect(resultAction).toEqual(expectedAction);
+        })
+    })
+
+    describe("addCashToWallet", () => {
+        it("should create action to add cash to wallet", () => {
+            const expectedAction = {
+                type: actions.ADD_CASH,
+                dollars: 100
+            }
+            const resultAction = actions.addCashToWallet(100);
+            expect(resultAction).toEqual(expectedAction);
+        })
+    })
+
+    describe("subtractCashFromWallet", () => {
+        it("should create action to subtract cash from wallet", () => {
+            const expectedAction = {
+                type: actions.SUBTRACT_CASH,
+                dollars: 100
+            }
+            const resultAction = actions.subtractCashFromWallet(100);
+            expect(resultAction).toEqual(expectedAction);
+        })
+    })
+})
